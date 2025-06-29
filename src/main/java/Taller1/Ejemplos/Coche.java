381e3463b7b55d31aa4df407a592747702379911
KevinGr <kgomezr2@unicartagena.edu.co>
2025-06-29 16:20:37 -0500

    feat: kevin manuel gomez rojas - ejemplo 2.2 constructor por parametrizado taller 1

--- src/main/java/Taller1/Ejemplos/Coche.java
+++ src/main/java/Taller1/Ejemplos/Coche.java
@@ -3,8 +3,8 @@ package Taller1.Ejemplos;
 public class Coche {
     String marca;
 
-    // Constructor por defecto
-    public Coche(){
-        marca = "Desconocida";
+    // Constructor parametrizado
+    public Coche(String marca) {
+        this.marca = marca;
     }
 }

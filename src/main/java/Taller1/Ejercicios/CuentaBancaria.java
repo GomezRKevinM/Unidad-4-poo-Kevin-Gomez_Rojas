a1cc2765c614edae0569f0423023c754138ebaf0
KevinGr <kgomezr2@unicartagena.edu.co>
2025-06-29 16:50:49 -0500

    feat: kevin manuel gomez rojas - Ejercicio 2: Crea una clase CuentaBancaria | taller 1

---
+++ src/main/java/Taller1/Ejercicios/CuentaBancaria.java
@@ -0,0 +1,24 @@
+package Taller1.Ejercicios;
+
+public class CuentaBancaria {
+    String numeroCuenta;
+    Long saldo;
+    String tipoCuenta;
+
+    public CuentaBancaria(){
+        numeroCuenta = "00000000";
+        saldo = 0L;
+        tipoCuenta = "Ahorros";
+    }
+
+    public CuentaBancaria(String numeroCuenta, String tipoCuenta) {
+        this.numeroCuenta = numeroCuenta;
+        this.tipoCuenta = tipoCuenta;
+    }
+
+    public CuentaBancaria (String numeroCuenta, Long saldo, String tipoCuenta) {
+        this(numeroCuenta, tipoCuenta);
+        this.saldo = saldo;
+    }
+
+}

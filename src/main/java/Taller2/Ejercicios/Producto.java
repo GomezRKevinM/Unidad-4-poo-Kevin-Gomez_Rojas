a4048efde466f51846dc589ed16731b2523a320b
KevinGr <kgomezr2@unicartagena.edu.co>
2025-06-30 13:52:03 -0500

    feat: kevin manuel gomez rojas - Ejercicio 1: Clase producto con constructor y metodos de acceso | taller2

---
+++ src/main/java/Taller2/Ejercicios/Producto.java
@@ -0,0 +1,16 @@
+package Taller2.Ejercicios;
+
+public class Producto {
+    private String nombre;
+    private double precio;
+
+    public Producto(String nombre, double precio) {
+        this.nombre = nombre;
+        this.precio = precio;
+    }
+
+    public void mostrarProducto() {
+        System.out.println("Nombre del producto: " + this.nombre);
+        System.out.println("Precio del producto: " + this.precio);
+    }
+}

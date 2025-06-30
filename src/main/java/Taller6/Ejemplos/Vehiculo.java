9eabb304e7777c394a9eb9b8398843fa87c6e1c5
KevinGr <kgomezr2@unicartagena.edu.co>
2025-06-30 18:06:54 -0500

    restaurando taller6 en rama principal

--- src/main/java/Taller6/Ejemplos/Vehiculo.java
+++ src/main/java/Taller6/Ejemplos/Vehiculo.java
@@ -1,6 +1,7 @@
 package Taller6.Ejemplos;
 
 public class Vehiculo {
+<<<<<<< HEAD
     protected String marca;
     protected double velocidad;
     public Vehiculo(String marca, double velocidad) {
@@ -9,5 +10,13 @@ public class Vehiculo {
     }
     protected void mostrarDetalles() {
         System.out.println("Marca: " + marca + ", Velocidad: " + velocidad + " km/h");
+=======
+    protected String tipo;
+    public Vehiculo(String tipo) {
+        this.tipo = tipo;
+    }
+    protected void mostrarTipo() {
+        System.out.println("Tipo de vehículo: " + tipo);
+>>>>>>> 67152bc (feat: kevin manuel gomez rojas - Ejemplo 1 Taller 6)
     }
 }

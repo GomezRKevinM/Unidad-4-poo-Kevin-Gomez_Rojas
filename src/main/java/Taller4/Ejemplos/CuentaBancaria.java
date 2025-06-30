7220799139f9f3a5987bc3acea25f85fecb07a72
KevinGr <kgomezr2@unicartagena.edu.co>
2025-06-30 15:27:10 -0500

    feat: kevin manuel gomez rojas - Ejemplo de encapsulamiento con private taller 4

---
+++ src/main/java/Taller4/Ejemplos/CuentaBancaria.java
@@ -0,0 +1,21 @@
+package Taller4.Ejemplos;
+
+public class CuentaBancaria {
+    private double saldo; // Atributo privado para proteger el saldo
+    public CuentaBancaria(double saldoInicial) {
+        if (saldoInicial >= 0) {
+            this.saldo = saldoInicial;
+        } else {
+            this.saldo = 0;
+        }
+    }
+    public double getSaldo() {
+        return saldo;
+    }
+
+    public void depositar(double monto){
+        if (monto > 0) {
+            saldo += monto;
+        }
+    }
+}

e5bebfde81d2e6205e159462889d3ae6bcbc97e1
KevinGr <kgomezr2@unicartagena.edu.co>
2025-06-30 14:22:30 -0500

    feat: kevin manuel gomez rojas - Ejemplo correcto 2: Metodo de utilidad static taller 3

--- src/main/java/Taller3/Ejemplos/main.java
+++ src/main/java/Taller3/Ejemplos/main.java
@@ -2,10 +2,7 @@ package Taller3.Ejemplos;
 
 public class main {
     public static void main(String[] args) {
-        Contador c1 = new Contador();
-        Contador c2 = new Contador();
-        Contador c3 = new Contador();
-        // Se accede al método estático usando el nombre de la clase
-        Contador.mostrarContador(); // Muestra: Contador global: 3
+        int resultado = Calculadora.sumar(5, 7); // Llama al método estático sin crear una instancia
+        System.out.println("Resultado: " + resultado); // Muestra: Resultado: 12
     }
 }

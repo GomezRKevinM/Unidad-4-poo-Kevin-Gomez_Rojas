2af91a1ca94e6203ea5cb368112ecbf55b93beb0
KevinGr <kgomezr2@unicartagena.edu.co>
2025-06-30 14:31:24 -0500

    feat: kevin manuel gomez rojas - Ejercicio 1: Clase coche con contador de instancias | taller 3

---
+++ src/main/java/Taller3/Ejercicios/Coche.java
@@ -0,0 +1,17 @@
+package Taller3.Ejercicios;
+
+public class Coche {
+    private String marca;
+    private String modelo;
+    private static int contadorCoches = 0;
+
+    public Coche(String marca, String modelo) {
+        this.marca = marca;
+        this.modelo = modelo;
+        contadorCoches++;
+    }
+
+    public static void mostrarContadorCoches() {
+        System.out.println("Número total de coches creados: " + contadorCoches);
+    }
+}

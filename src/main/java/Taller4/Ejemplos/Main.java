7e41b55f15f39335581016df35e0cb869c866a78
KevinGr <kgomezr2@unicartagena.edu.co>
2025-06-30 15:33:01 -0500

    feat: kevin manuel gomez rojas - Ejemplo de uso incorrecto 2: No usar metodos get y set para controlar el acceso. taller 4

--- src/main/java/Taller4/Ejemplos/Main.java
+++ src/main/java/Taller4/Ejemplos/Main.java
@@ -3,6 +3,5 @@ package Taller4.Ejemplos;
 public class Main {
     public static void main(String[] args) {
         Persona p = new Persona("Juan");
-        System.out.println(p.nombre); // Error de compilación: nombre tiene acceso privado
     }
 }
---
+++ src/main/java/Taller4/Ejemplos/Producto.java
@@ -0,0 +1,12 @@
+package Taller4.Ejemplos;
+
+public class Producto {
+    private double precio;
+    public Producto(double precio) {
+        this.precio = precio;
+    }
+    public void actualizarPrecio(double nuevoPrecio) {
+    // No se realiza ninguna validación aquí
+        precio = nuevoPrecio;
+    }
+}

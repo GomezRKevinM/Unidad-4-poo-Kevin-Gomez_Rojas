c1e58ebe61a8ceef3e701124760c5f68c91acf68
KevinGr <kgomezr2@unicartagena.edu.co>
2025-06-30 18:50:15 -0500

    feat: kevin manuel gomez rojas - Ejercicio 1: Clase Vehiculo y Clase Coche taller 8

--- src/main/java/Taller8/Ejemplos/Perro.java
+++ src/main/java/Taller8/Ejemplos/Perro.java
@@ -2,6 +2,8 @@ package Taller8.Ejemplos;
 
 public class Perro extends Animal{
     public Perro(String raza) {
+        super("Canis lupus familiaris"); // Llama al constructor de la clase base Animal
+        // super(especie); // Alternativamente, si se desea especificar una especie diferente
     // No se llama al constructor de la clase base
         System.out.println("Raza: " + raza);
     }

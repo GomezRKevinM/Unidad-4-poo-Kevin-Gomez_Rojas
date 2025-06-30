d5207fdc4caf425f5d6a0c5f0f1d6e024806a2ec
KevinGr <kgomezr2@unicartagena.edu.co>
2025-06-30 18:11:06 -0500

    feat: kevin manuel gomez rojas - Ejemplo 1 taller 7

---
+++ src/main/java/Taller7/Ejemplos/Persona.java
@@ -0,0 +1,11 @@
+package Taller7.Ejemplos;
+
+public class Persona {
+    public String nombre;
+    public Persona(String nombre) {
+        this.nombre = nombre;
+    }
+    public void mostrarNombre() {
+        System.out.println("Nombre: " + nombre);
+    }
+}

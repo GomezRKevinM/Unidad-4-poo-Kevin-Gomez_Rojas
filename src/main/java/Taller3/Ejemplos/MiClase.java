6ad4c840b571f6b8990afbaa48e2e2bb606e15f8
KevinGr <kgomezr2@unicartagena.edu.co>
2025-06-30 14:08:55 -0500

    feat: kevin manuel gomez rojas - Ejemplo: ¿Como se define static? taller 3

---
+++ src/main/java/Taller3/Ejemplos/MiClase.java
@@ -0,0 +1,8 @@
+package Taller3.Ejemplos;
+
+public class MiClase {
+    static int contador = 0; // Atributo estático
+    static void incrementarContador() { // Método estático
+        contador++;
+    }
+}

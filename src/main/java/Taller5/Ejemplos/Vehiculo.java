35895677ebc6555ea71afe2fa72149a8ab5fb71a
KevinGr <kgomezr2@unicartagena.edu.co>
2025-06-30 16:18:53 -0500

    feat: kevin manuel gomez rojas - Ejemplo de uso incorrecto 1: Intentar acceder a un miembro de paquete desde fuera del paquete. Taller 5

---
+++ src/main/java/Taller5/Ejemplos/Vehiculo.java
@@ -0,0 +1,8 @@
+package Taller5.Ejemplos;
+
+public class Vehiculo {
+    String marca;
+    Vehiculo(String marca) {
+        this.marca = marca;
+    }
+}
---
+++ src/main/java/Taller5/Ejercicios/Main.java
@@ -0,0 +1,7 @@
+package Taller5.Ejercicios;
+
+public class Main {
+    public static void main(String[] args) {
+        Vehiculo v = new Vehiculo("Toyota"); // Error de compilación: Vehiculo no es visible
+    }
+}

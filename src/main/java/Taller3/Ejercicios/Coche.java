a161c67dc6447c4175a57bc4ea95e177bf74f2e0
KevinGr <kgomezr2@unicartagena.edu.co>
2025-06-30 15:17:56 -0500

    feat: kevin manuel gomez rojas - Ejercicio 3: Uso incorrecto de static | taller 3

--- src/main/java/Taller3/Ejercicios/Coche.java
+++ src/main/java/Taller3/Ejercicios/Coche.java
@@ -14,4 +14,12 @@ public class Coche {
     public static void mostrarContadorCoches() {
         System.out.println("Número total de coches creados: " + contadorCoches);
     }
+
+    public static void setModelo(String modelo) {
+        // Este método no tiene sentido en el contexto de la clase Coche,
+        // ya que no se puede establecer un modelo estático para todos los coches.
+        // Cada coche debería tener su propio modelo.
+        System.out.println("No se puede establecer un modelo estático para todos los coches.");
+        this.modelo = modelo; // Error de compilación: 'this' no se puede usar en un contexto estático porq this hace referencia a una instancia específica de la clase y static es un metodo global.
+    }
 }

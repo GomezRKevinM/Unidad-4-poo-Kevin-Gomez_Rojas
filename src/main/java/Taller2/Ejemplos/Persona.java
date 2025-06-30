4e67fd135bcdc7cc6d7a59afeff689034432b660
KevinGr <kgomezr2@unicartagena.edu.co>
2025-06-30 13:16:09 -0500

    feat: kevin manuel gomez rojas - Ejemplo simple de this para diferenciar atributos y parámetros Taller 2

---
+++ src/main/java/Taller2/Ejemplos/Persona.java
@@ -0,0 +1,11 @@
+package Taller2.Ejemplos;
+
+public class Persona {
+    private String nombre;
+    public Persona(String nombre) {
+        this.nombre = nombre; // Usamos *this* para referirnos al atributo de la clase
+    }
+    public void mostrarNombre() {
+        System.out.println("Nombre: " + this.nombre); // Usamos *this* para referirnos al atributo
+    }
+}

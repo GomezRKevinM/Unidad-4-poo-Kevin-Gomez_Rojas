79300d412608fd857b94ca1209cce816bce2d434
KevinGr <kgomezr2@unicartagena.edu.co>
2025-06-30 17:19:05 -0500

    feat: kevin manuel gomez rojas - Ejemplo de uso correcto 1: Atributos protected en una clase base taller 6

--- src/main/java/Taller6/Ejemplos/Vehiculo.java
+++ src/main/java/Taller6/Ejemplos/Vehiculo.java
@@ -1,7 +1,6 @@
 package Taller6.Ejemplos;
 
 public class Vehiculo {
-<<<<<<< HEAD
     protected String marca;
     protected double velocidad;
     public Vehiculo(String marca, double velocidad) {
@@ -10,13 +9,5 @@ public class Vehiculo {
     }
     protected void mostrarDetalles() {
         System.out.println("Marca: " + marca + ", Velocidad: " + velocidad + " km/h");
-=======
-    protected String tipo;
-    public Vehiculo(String tipo) {
-        this.tipo = tipo;
-    }
-    protected void mostrarTipo() {
-        System.out.println("Tipo de vehículo: " + tipo);
->>>>>>> 67152bc (feat: kevin manuel gomez rojas - Ejemplo 1 Taller 6)
     }
 }

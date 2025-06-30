101cda793f2065fa2e9c9388871962889311b9d3
KevinGr <kgomezr2@unicartagena.edu.co>
2025-06-30 13:48:53 -0500

    feat: kevin manuel gomez rojas - Ejemplo de uso incorrecto 2: Uso innecesario de this sin ambiguedad taller 2

--- src/main/java/Taller2/Ejemplos/Persona.java
+++ src/main/java/Taller2/Ejemplos/Persona.java
@@ -2,7 +2,12 @@ package Taller2.Ejemplos;
 
 public class Persona {
     private String nombre;
-    public static void mostrarNombre() {
-        System.out.println(this.nombre); // Error de compilación: No se puede usar `this` en un contexto estático
+    public void setNombre(String nombre) {
+        this.nombre = nombre; // Aquí `this` es necesario
+// Uso innecesario de `this` dentro de un método sin ambigüedad
+        this.mostrarNombre(); // Aunque compila, `this` aquí es innecesario
+    }
+    public void mostrarNombre() {
+        System.out.println("Nombre: " + nombre); // `this` no es necesario aquí
     }
 }

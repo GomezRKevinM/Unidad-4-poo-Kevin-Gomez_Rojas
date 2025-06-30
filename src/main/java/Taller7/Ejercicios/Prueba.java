90ebd7892f0ac7f64982bb489a48292514f719be
KevinGr <kgomezr2@unicartagena.edu.co>
2025-06-30 18:27:08 -0500

    feat: kevin manuel gomez rojas - Ejercicio 2: Clase CuentaBancaria taller 7

--- src/main/java/Taller7/Ejercicios/Prueba.java
+++ src/main/java/Taller7/Ejercicios/Prueba.java
@@ -2,16 +2,17 @@ package Taller7.Ejercicios;
 
 public class Prueba {
     public static void main(String[] args) {
-        Empleado empleado = new Empleado("Kevin", 100000.00);
-        System.out.println("Nombre del empleado: " + empleado.nombre);
-        empleado.nombre = "Juan";
-        System.out.println("Nuevo nombre del empleado: " + empleado.nombre);
-        empleado.setSalario(150000.00);
-        System.out.println("Nuevo salario del empleado: " + empleado.getSalario());
+        CuentaBancaria cuenta = new CuentaBancaria("123456789", 1000.0, "Ahorros");
+        cuenta.mostrarDetalles();
+        System.out.println("Saldo actual: " + cuenta.getSaldo());
+        cuenta.setSaldo(1500.0);
+        System.out.println("Nuevo saldo: " + cuenta.getSaldo());
 
+        cuenta.numeroCuenta = "987654321"; // Esto es posible porque numeroCuenta es privado
         /**
-         * Podemos ver que el atributo nombre es público y se puede acceder directamente,
-         * pero el atributo salario es privado y solo se puede acceder a través de los métodos públicos getSalario() y setSalario().
+         * Si se intenta acceder directamente a numeroCuenta desde fuera de la clase,
+         * se producirá un error de compilación, ya que es un campo privado.
+         * output: java: numeroCuenta has private access in Taller7.Ejercicios.CuentaBancaria
          */
     }
 }

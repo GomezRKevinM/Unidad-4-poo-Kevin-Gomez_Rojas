b0605fc6bac36804520be6b18f5e48465576fbff
KevinGr <kgomezr2@unicartagena.edu.co>
2025-06-30 18:18:21 -0500

    feat: kevin manuel gomez rojas - Ejemplo de uso incorrecto 2: Exposición pública de Atributos sensibles. taller 7

--- src/main/java/Taller7/Ejemplos/paquete2/Main.java
+++
@@ -1,7 +0,0 @@
-package Taller7.Ejemplos.paquete2;
-
-public class Main {
-    public static void main(String[] args) {
-        Persona p = new Persona("Juan"); // Error de compilación: Persona no es visible
-    }
-}

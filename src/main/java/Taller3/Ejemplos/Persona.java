7a14bfa1b9d34796a8f0acbdf1a14ee932d40f1b
KevinGr <kgomezr2@unicartagena.edu.co>
2025-06-30 14:28:03 -0500

    feat: kevin manuel gomez rojas - Ejemplo de uso incorrecto 2: Declarar metodos static que modifican datos de instancia taller 3

--- src/main/java/Taller3/Ejemplos/Persona.java
+++ src/main/java/Taller3/Ejemplos/Persona.java
@@ -2,7 +2,7 @@ package Taller3.Ejemplos;
 
 public class Persona {
     private String nombre;
-    public static void mostrarNombre() {
+    public void mostrarNombre() {
         System.out.println("Nombre: " + nombre); // Error de compilación: No se puede acceder a 'nombre' desde un contexto estático
     }
 }

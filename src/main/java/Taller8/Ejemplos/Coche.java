fd720a04fa45080c38af6db3e311f45487bc1fd2
KevinGr <kgomezr2@unicartagena.edu.co>
2025-06-30 18:46:18 -0500

    feat: kevin manuel gomez rojas - Ejemplo de uso incorrecto 3: No usar super en el constructor taller 8

--- src/main/java/Taller8/Ejemplos/Coche.java
+++ src/main/java/Taller8/Ejemplos/Coche.java
@@ -2,6 +2,6 @@ package Taller8.Ejemplos;
 
 public class Coche {
     public void mostrarMarca() {
-        System.out.println(marca); // Error de compilación: 'marca' tiene acceso privado
+        // System.out.println(marca); // Error de compilación: 'marca' tiene acceso privado
     }
 }

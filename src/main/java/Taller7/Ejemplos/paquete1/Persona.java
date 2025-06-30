52fb3d5e661bc1200e7e2824f0fa793a55170ffd
KevinGr <kgomezr2@unicartagena.edu.co>
2025-06-30 18:16:09 -0500

    feat: kevin manuel gomez rojas - Ejemplo de uso incorrecto 1: Intentar acceder a una clase no publica desde fuera del paquete. taller 7

--- src/main/java/Taller7/Ejemplos/paquete1/Persona.java
+++ src/main/java/Taller7/Ejemplos/paquete1/Persona.java
@@ -1,11 +1,8 @@
-package Taller7.Ejemplos;
+package Taller7.Ejemplos.paquete1;
 
 public class Persona {
     public String nombre;
     public Persona(String nombre) {
         this.nombre = nombre;
-    }
-    public void mostrarNombre() {
-        System.out.println("Nombre: " + nombre);
     }
 }
---
+++ src/main/java/Taller7/Ejemplos/paquete2/Main.java
@@ -0,0 +1,7 @@
+package Taller7.Ejemplos.paquete2;
+
+public class Main {
+    public static void main(String[] args) {
+        Persona p = new Persona("Juan"); // Error de compilación: Persona no es visible
+    }
+}

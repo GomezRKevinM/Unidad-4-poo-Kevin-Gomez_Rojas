60cf6f4b236a29483176837ee2ea3d2b46a66268
KevinGr <kgomezr2@unicartagena.edu.co>
2025-06-30 15:31:32 -0500

    feat: kevin manuel gomez rojas - Ejemplo de uso incorrecto 1: Intentar acceder a un miembro privado desde otra clase. taller 4

---
+++ src/main/java/Taller4/Ejemplos/Main.java
@@ -0,0 +1,8 @@
+package Taller4.Ejemplos;
+
+public class Main {
+    public static void main(String[] args) {
+        Persona p = new Persona("Juan");
+        System.out.println(p.nombre); // Error de compilación: nombre tiene acceso privado
+    }
+}
--- src/main/java/Taller4/Ejemplos/Persona.java
+++ src/main/java/Taller4/Ejemplos/Persona.java
@@ -2,23 +2,7 @@ package Taller4.Ejemplos;
 
 public class Persona {
     private String nombre;
-    private int edad;
-    public Persona(String nombre, int edad) {
+    public Persona(String nombre) {
         this.nombre = nombre;
-        this.edad = edad;
-    }
-    public String getNombre() {
-        return nombre;
-    }
-    public void setNombre(String nombre) {
-        this.nombre = nombre;
-    }
-    public int getEdad() {
-        return edad;
-    }
-    public void setEdad(int edad) {
-        if (edad >= 0) {
-            this.edad = edad;
-        }
     }
 }

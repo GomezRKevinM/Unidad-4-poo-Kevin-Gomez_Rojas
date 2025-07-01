2fcbc545b032c2ac360e3905c982171c004e78ff
KevinGr <kgomezr2@unicartagena.edu.co>
2025-06-30 19:55:29 -0500

    feat: kevin manuel gomez rojas - Ejercicio 3.1 taller 10

--- src/main/java/Taller10/Ejercicios/Bicicleta.java
+++ src/main/java/Taller10/Ejercicios/Bicicleta.java
@@ -2,7 +2,11 @@ package Taller10.Ejercicios;
 
 public class Bicicleta extends Vehiculo{
     @Override
-    public void moverse() {
-        System.out.println("La bicicleta se está moviendo.");
+    public void moverse(String tipoMovimiento) {
+        System.out.println("La bicicleta se está moviendo de forma " + tipoMovimiento + ".");
     }
+
+    /**
+     * Este metodo sobrescribe el método moverse de la clase Vehiculo, pero no funciona porque la firma del método es diferente.
+     */
 }

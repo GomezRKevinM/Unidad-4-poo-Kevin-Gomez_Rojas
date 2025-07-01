ee14e22ebc60c334a2f08f340a36f95dc41c01b3
KevinGr <kgomezr2@unicartagena.edu.co>
2025-07-01 12:18:22 -0500

    feat: kevin manuel gomez Rojas - Ejercicio 3.2 taller 12

--- src/main/java/Taller12/Ejercicios/Incorrecto.java
+++ src/main/java/Taller12/Ejercicios/Incorrecto.java
@@ -1,8 +1,14 @@
 package Taller12.Ejercicios;
 
 public class Incorrecto implements Respirador{
+    @Override
+    public void respirar() {
+        System.out.println("El pez está respirando.");
+    }
 
-    // No implementa el método nadar de la interfaz Nadador
-    // No implementa el método respirar de la interfaz Respirador
-    // Esto es incorrecto porque la clase Incorrecto debería implementar todos los métodos de las interfaces que declara.
+    /**
+     * No tiene sentido implementar el metodo respirar en la clase Incorrecto,
+     * ya que esta clase no representa ninguna clase que necesite respirar o usar los metodos de la interfaz Respirador.
+     * Esto puede llevar a confusiones y errores en el diseño del programa.
+     */
 }

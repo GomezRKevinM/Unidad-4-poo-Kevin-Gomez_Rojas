6187bad39af8736c865cd2a178898e65343d340d
KevinGr <kgomezr2@unicartagena.edu.co>
2025-06-30 19:36:23 -0500

    feat: kevin manuel gomez rojas - Ejercicio 3.2 taller 9

--- src/main/java/Taller9/Ejemplos/Main.java
+++ src/main/java/Taller9/Ejemplos/Main.java
@@ -2,10 +2,6 @@ package Taller9.Ejemplos;
 
 public class Main {
     public static void main(String[] args) {
-        // Error: No se puede usar `super` aquí porque Main no es una clase derivada de ninguna otra clase.
-        super.tipo = "Orquídea"; // Error de compilación
-        /**
-         * Error: java: non-static variable super cannot be referenced from a static context
-         */
+
     }
 }

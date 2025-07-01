9307fb23ba1d081cbbea88f402aa0ff504426b67
KevinGr <kgomezr2@unicartagena.edu.co>
2025-07-01 12:16:36 -0500

    feat: kevin manuel gomez rojas - Ejercicio 3.1 taller 12

---
+++ src/main/java/Taller12/Ejercicios/Incorrecto.java
@@ -0,0 +1,8 @@
+package Taller12.Ejercicios;
+
+public class Incorrecto implements Respirador{
+
+    // No implementa el método nadar de la interfaz Nadador
+    // No implementa el método respirar de la interfaz Respirador
+    // Esto es incorrecto porque la clase Incorrecto debería implementar todos los métodos de las interfaces que declara.
+}

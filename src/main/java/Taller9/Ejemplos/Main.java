5b0601c03fa79e3e545bb391ecea707b046e0e3a
KevinGr <kgomezr2@unicartagena.edu.co>
2025-06-30 19:34:43 -0500

    feat: kevin manuel gomez rojas - Ejercicio 3.1 taller 9

--- src/main/java/Taller9/Ejemplos/Main.java
+++ src/main/java/Taller9/Ejemplos/Main.java
@@ -2,7 +2,10 @@ package Taller9.Ejemplos;
 
 public class Main {
     public static void main(String[] args) {
-        // Error: No se puede usar `super` aquí porque Main no es una clase derivada de Planta
+        // Error: No se puede usar `super` aquí porque Main no es una clase derivada de ninguna otra clase.
         super.tipo = "Orquídea"; // Error de compilación
+        /**
+         * Error: java: non-static variable super cannot be referenced from a static context
+         */
     }
 }
---
+++ src/main/java/Taller9/Ejercicios/Main.java
@@ -0,0 +1,12 @@
+package Taller9.Ejercicios;
+
+public class Main {
+    public static void main(String[] args) {
+        super();
+        Animal animal = new Animal("Perro");
+        animal.mostrarEspecie();
+
+        Pez pez = new Pez("Tiburón", "Salado");
+        pez.mostrarEspecie();
+    }
+}

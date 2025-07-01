37786d8b1f4c7d6312a90897fe938f6c713ea282
KevinGr <kgomezr2@unicartagena.edu.co>
2025-07-01 11:34:48 -0500

    feat: kevin manuel gomez rojas - Ejercicio 3: Uso incorrecto de clases abstractas taller 11

---
+++ src/main/java/Taller11/Ejercicios/Main.java
@@ -0,0 +1,14 @@
+package Taller11.Ejercicios;
+
+public class Main {
+    public static void main(String[] args) {
+        Abstracta claseAbstracta = new Abstracta("Información de la clase abstracta");
+
+        /**
+         * Al instanciar una clase abstracta, se debe proporcionar un argumento no nulo y no vacío.
+         * * Si se intenta crear una instancia de una clase abstracta sin proporcionar un argumento válido,
+         * se lanzará una excepción IllegalArgumentException.
+         * output: java: Taller11.Ejercicios.Abstracta is abstract; cannot be instantiated
+         */
+    }
+}

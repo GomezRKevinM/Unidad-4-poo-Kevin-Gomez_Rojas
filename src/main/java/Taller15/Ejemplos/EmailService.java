ff0cf7217e5ccadeb21610e37a413f8f203df554
KevinGr <kgomezr2@unicartagena.edu.co>
2025-07-01 16:09:25 -0500

    feat: kevin manuel gomez rojas - Ejemplo 3: Sistema de notificaciones taller 15

---
+++ src/main/java/Taller15/Ejemplos/EmailService.java
@@ -0,0 +1,8 @@
+package Taller15.Ejemplos;
+
+public class EmailService {
+    public void enviarEmail(String destinatario, String mensaje) {
+        System.out.println("Enviando email a: " + destinatario);
+        System.out.println("Mensaje: " + mensaje);
+    }
+}
---
+++ src/main/java/Taller15/Ejemplos/SMSService.java
@@ -0,0 +1,8 @@
+package Taller15.Ejemplos;
+
+public class SMSService {
+    public void enviarSMS(String numero, String mensaje) {
+        System.out.println("Enviando SMS a: " + numero);
+        System.out.println("Mensaje: " + mensaje);
+    }
+}

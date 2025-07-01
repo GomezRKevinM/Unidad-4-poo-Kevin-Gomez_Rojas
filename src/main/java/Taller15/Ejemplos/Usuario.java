e47645f13fdb1e1c5c04134b391a0409ab3517cb
KevinGr <kgomezr2@unicartagena.edu.co>
2025-07-01 16:05:38 -0500

    feat: kevin manuel gomez rojas - Ejemplo 1: Gestion de usuarios en una aplicacion taller 15

---
+++ src/main/java/Taller15/Ejemplos/Usuario.java
@@ -0,0 +1,13 @@
+package Taller15.Ejemplos;
+
+public class Usuario {
+    private String nombre;
+    private String contrasena;
+    public Usuario(String nombre, String contrasena) {
+        this.nombre = nombre;
+        this.contrasena = contrasena;
+    }
+    public String getNombre() {
+        return nombre;
+    }
+}

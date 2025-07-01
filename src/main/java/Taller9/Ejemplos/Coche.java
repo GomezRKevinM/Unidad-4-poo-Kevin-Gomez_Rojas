65735cc25c469b834b366c791b74792dbea0bec7
KevinGr <kgomezr2@unicartagena.edu.co>
2025-06-30 19:18:45 -0500

    feat: kevin manuel gomez rojas - Ejemplo uso del super para invocar Constructores de la clase Base taller 9

---
+++ src/main/java/Taller9/Ejemplos/Coche.java
@@ -0,0 +1,10 @@
+package Taller9.Ejemplos;
+
+public class Coche extends Vehiculo{
+    private int numeroPuertas;
+
+    public Coche(String marca, int numeroPuertas) {
+        super(marca); // Llama al constructor de la clase base
+        this.numeroPuertas = numeroPuertas;
+    }
+}
---
+++ src/main/java/Taller9/Ejemplos/Vehiculo.java
@@ -0,0 +1,8 @@
+package Taller9.Ejemplos;
+
+public class Vehiculo {
+    protected String marca;
+    public Vehiculo(String marca) {
+        this.marca = marca;
+    }
+}

65b4ad9497e2c6ac51a98214d6b6e892f6c120bc
KevinGr <kgomezr2@unicartagena.edu.co>
2025-07-01 13:59:02 -0500

    feat: kevin manuel gomez rojas - Ejercicio 1: Clase figura y clases derivadas Circulo y Rectangulo taller 14

---
+++ src/main/java/Taller14/Ejercicios/Main.java
@@ -0,0 +1,10 @@
+package Taller14.Ejercicios;
+
+public class Main {
+    public static void main(String[] args) {
+        Circulo miCirculo = new Circulo(23);
+        Rectangulo miRectangulo = new Rectangulo(10, 20);
+        miCirculo.calcularArea();
+        miRectangulo.calcularArea();
+    }
+}

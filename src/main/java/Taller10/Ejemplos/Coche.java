0d9360e2fff9af0a9756bb26d8dc6a839f17dfed
KevinGr <kgomezr2@unicartagena.edu.co>
2025-06-30 19:48:56 -0500

    feat: kevin manuel gomez rojas - Ejemplo de uso incorrecto 2: Sobrescribir un metodo sin usar @Override taller 10

--- src/main/java/Taller10/Ejemplos/Coche.java
+++ src/main/java/Taller10/Ejemplos/Coche.java
@@ -3,7 +3,7 @@ package Taller10.Ejemplos;
 public class Coche extends Vehiculo{
     // Error de compilación: Diferente firma del método
     @Override
-    public void acelerar() {
-        System.out.println("El coche acelera.");
+    public void acelerar(int velocidad) {
+        System.out.println("El coche acelera a " + velocidad + " km/h.");
     }
 }
---
+++ src/main/java/Taller10/Ejemplos/Gato.java
@@ -0,0 +1,7 @@
+package Taller10.Ejemplos;
+
+public class Gato extends Animal{
+    public void hacerSonido() {
+        System.out.println("El gato maúlla.");
+    }
+}

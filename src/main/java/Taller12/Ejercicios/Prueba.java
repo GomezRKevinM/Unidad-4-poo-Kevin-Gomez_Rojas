3a4e4d3e5cdcf5dcc4d3a2c591390c3f45506804
KevinGr <kgomezr2@unicartagena.edu.co>
2025-07-01 12:11:30 -0500

    feat: kevin manuel gomez rojas - Ejercicio 1: Clase Ave con interfaces Volador y Cantante taller 12

---
+++ src/main/java/Taller12/Ejercicios/Prueba.java
@@ -0,0 +1,9 @@
+package Taller12.Ejercicios;
+
+public class Prueba {
+    public static void main(String[] args) {
+        Ave pajaro = new Ave();
+        pajaro.volar(); // El ave está volando.
+        pajaro.cantar(); // El ave está cantando.
+    }
+}
---
+++ src/main/java/Taller12/Ejercicios/Volador.java
@@ -0,0 +1,5 @@
+package Taller12.Ejercicios;
+
+public interface Volador {
+    void volar();
+}

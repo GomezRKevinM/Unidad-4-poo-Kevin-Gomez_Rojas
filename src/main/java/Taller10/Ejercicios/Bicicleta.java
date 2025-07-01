8da1193edb81bf7afa7f222be6633ed7708de7d0
KevinGr <kgomezr2@unicartagena.edu.co>
2025-06-30 19:54:14 -0500

    feat: kevin manuel gomez rojas - Ejercicio 2: Clase Vehiculo y Bicicleta taller 10

---
+++ src/main/java/Taller10/Ejercicios/Bicicleta.java
@@ -0,0 +1,8 @@
+package Taller10.Ejercicios;
+
+public class Bicicleta extends Vehiculo{
+    @Override
+    public void moverse() {
+        System.out.println("La bicicleta se está moviendo.");
+    }
+}
--- src/main/java/Taller10/Ejercicios/Prueba.java
+++ src/main/java/Taller10/Ejercicios/Prueba.java
@@ -2,12 +2,10 @@ package Taller10.Ejercicios;
 
 public class Prueba {
     public static void main(String[] args) {
-        Persona persona = new Persona();
-        Estudiante estudiante = new Estudiante();
-        Profesor profesor = new Profesor();
+        Vehiculo vehiculo = new Vehiculo();
+        Bicicleta bicicleta = new Bicicleta();
 
-        persona.presentarse(); // Salida: Hola, soy una persona.
-        estudiante.presentarse(); // Salida: Hola, soy un estudiante.
-        profesor.presentarse(); // Salida: Hola, soy un profesor.
+        vehiculo.moverse();
+        bicicleta.moverse();
     }
 }

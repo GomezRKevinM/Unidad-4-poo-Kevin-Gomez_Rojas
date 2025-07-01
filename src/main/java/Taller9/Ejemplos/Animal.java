d1f7d35c677c375d79660a445dcadaf6209ddf5a
KevinGr <kgomezr2@unicartagena.edu.co>
2025-06-30 19:19:50 -0500

    feat: kevin manuel gomez rojas - Ejemplo correcto 1: Invocar el constructor de la clase Base taller 9

--- src/main/java/Taller9/Ejemplos/Animal.java
+++ src/main/java/Taller9/Ejemplos/Animal.java
@@ -5,4 +5,7 @@ public class Animal {
     public Animal(String especie) {
         this.especie = especie;
     }
+    public void mostrarEspecie() {
+        System.out.println("Especie: " + especie);
+    }
 }

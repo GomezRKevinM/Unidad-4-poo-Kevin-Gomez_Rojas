dbd38aeb620ec11d0e106d54303b4bf24ee029f3
KevinGr <kgomezr2@unicartagena.edu.co>
2025-07-01 13:55:16 -0500

    feat: kevin manuel gomez rojas - Ejemplo uso incorrecto 2: Uso innecesario de polimorfismo taller 14

--- src/main/java/Taller14/Ejemplos/Main.java
+++ src/main/java/Taller14/Ejemplos/Main.java
@@ -3,6 +3,5 @@ package Taller14.Ejemplos;
 public class Main {
     public static void main(String[] args) {
         Animal miAnimal = new Perro();
-        miAnimal.correr(); // Error de compilación: El método correr() no existe en Animal
     }
 }

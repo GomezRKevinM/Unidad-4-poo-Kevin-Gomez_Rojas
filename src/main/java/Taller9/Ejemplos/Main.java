3dae79b25af54b161f52652ddf61fa400f66f4c6
KevinGr <kgomezr2@unicartagena.edu.co>
2025-06-30 19:23:02 -0500

    feat: kevin manuel gomez rojas - Ejemplo de uso incorrecto 1: Intentar User super Fuera del contexto de una clase derivada taller 9

---
+++ src/main/java/Taller9/Ejemplos/Main.java
@@ -0,0 +1,8 @@
+package Taller9.Ejemplos;
+
+public class Main {
+    public static void main(String[] args) {
+        // Error: No se puede usar `super` aquí porque Main no es una clase derivada de Planta
+        super.tipo = "Orquídea"; // Error de compilación
+    }
+}

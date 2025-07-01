e3aae0a1a6c4a312228044e9cb7b108d51419baf
KevinGr <kgomezr2@unicartagena.edu.co>
2025-07-01 13:05:59 -0500

    feat: kevin manuel gomez rojas - Ejercicio 2: Interfaces Volador y Nadador taller 13

--- src/main/java/Taller13/Ejercicios/Prueba.java
+++ src/main/java/Taller13/Ejercicios/Prueba.java
@@ -2,10 +2,8 @@ package Taller13.Ejercicios;
 
 public class Prueba {
     public static void main(String[] args) {
-        Rectangulo rectangulo = new Rectangulo(5, 10);
-        Triangulo triangulo = new Triangulo(10, 15);
-
-        System.out.println("Área del rectángulo: " + rectangulo.calcularArea());
-        System.out.println("Área del triángulo: " + triangulo.calcularArea());
+        Pato pato = new Pato();
+        pato.volar(); // El pato está volando.
+        pato.nadar(); // El pato está nadando.
     }
 }

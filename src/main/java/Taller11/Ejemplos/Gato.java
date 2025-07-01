bb64c1168cb44b0b64d3ab8b7ccd9c66a2269c7c
KevinGr <kgomezr2@unicartagena.edu.co>
2025-07-01 09:38:35 -0500

    feat: kevin manuel gomez rojas - Ejemplo incorrecto 2: Definir metodos abstractos que no son sobrescritos taller 11

---
+++ src/main/java/Taller11/Ejemplos/Gato.java
@@ -0,0 +1,5 @@
+package Taller11.Ejemplos;
+
+public class Gato extends Animal {
+    // Aunque el método hacerSonido no es sobrescrito, se permite debido a la falta de visibilidad o error lógico
+}
